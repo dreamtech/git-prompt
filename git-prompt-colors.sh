23c70f0471915deabe61bd63a8fd8614053a80c1
Ranfis Sanchez <ranfis.sh@gmail.com>
2014-06-16 15:25:28 -0400

    Change symbol for conflicted files

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -15,7 +15,7 @@ GIT_PROMPT_SEPARATOR="|"              # separates each item
 
 GIT_PROMPT_BRANCH="${White}"        # the git branch that is active in the current directory
 GIT_PROMPT_STAGED="${Yellow}●"           # the number of staged files/directories
-GIT_PROMPT_CONFLICTS="${Red}✕"        # the number of files in conflict
+GIT_PROMPT_CONFLICTS="${Red}x"        # the number of files in conflict
 GIT_PROMPT_CHANGED="${Red}+"         # the number of changed files
 
 GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
